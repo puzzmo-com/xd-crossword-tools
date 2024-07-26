018cd0954e0b73003f660d1e915c2a82efca0bbf
cod1r <jasonho353@gmail.com>
2024-07-26 00:11:36 -0500

    exported a function (getXDClueAnswer)
    the exported function helps convert a JSON clue to it's XD clue

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export * from "./lib/types"
 
-export { JSONToXD } from "./lib/JSONtoXD"
+export { JSONToXD, getXDClueAnswer } from "./lib/JSONtoXD"
 export { puzToXD } from "./lib/puzToXD"
 export { xdParser as xdToJSON } from "./lib/xdparser2"
 export { uclickXMLToXd } from "./lib/uclickXMLToXd"
