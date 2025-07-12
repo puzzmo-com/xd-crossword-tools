c9544a2ca67772e3640d038eb39b56529a534204
Orta Therox <git@orta.io>
2025-07-12 10:50:43 +0100

    More examples

--- website/src/exampleXDs.ts
+++ website/src/exampleXDs.ts
@@ -68,11 +68,9 @@ D28. Winter clock setting on the LES ~ E|S|T
     note: "",
     xd: `## Metadata
 
-title: tbd
+title: Creature Feature
 author: Danger, ./orta, and brooke
-date: Not set
-editor: Not set
-dupe: true
+editor: Brooke Husic
 copyright: © 2023
 blacksquares: 16
 whitespaces: 84
@@ -206,6 +204,202 @@ D27 ^hint: Hair-styling substance
 
 D28. Winter clock setting on the LES ~ E|S|T
 D28 ^hint: Time zone that looks like a superlative suffix: Abbr.
+`,
+  },
+  {
+    title: "Pick a Side by Orta & Brooke",
+    note: "",
+    xd: `## Metadata
+
+title: Pick a Side
+author: orta & brooke
+date: Not set
+editor: Not set
+copyright: © 2024
+blacksquares: 30
+whitespaces: 114
+acrossclues: 24
+downclues: 24
+width: 12
+height: 12
+size: 12x12
+splitcharacter: |
+highlights: A13 A15 D2 
+
+## Grid
+
+.DATA.BSIDES
+DINOS.ALMOST
+MENUS.LIMITS
+STAR..ADELE.
+...DESTINY..
+..CAVERNS...
+.HETERO.ESQ.
+HELEN....TUB
+ACE.SAW.AREA
+DASH.PARKOUR
+ETTA.EREADER
+SEED.XYZ.ESE
+
+## Clues
+
+A1. "Star Trek" android ~ DATA
+A1 ^hint: Spreadsheet contents
+
+A5. Secondary tracks on a record that give their name to harder versions of levels in 20-Down ~ B|SIDES
+A5 ^hint: Less-played songs whose first four letters anagram to "dibs": Hyph.
+A5 ^refs: D20
+
+A11. T. rex and dilophosaurus ~ DINOS
+A11 ^hint: "Jurassic Park" creatures: Abbr.
+
+A12. "Close, but not quite!" ~ ALMOST
+A12 ^hint: Word meaning "just about" whose last four letters spell the opposite of "least"
+
+A13. "The two types of games are ___ and 35-Across" (assertion in a 2024 Reddit post by imGhostKitty) ~ MENUS
+A13 ^hint: Lists with a restaurant's food offerings
+A13 ^refs: A35
+
+A14. Boundaries ~ LIMITS
+A14 ^hint: Speed ___ (highway maximums)
+
+A15. Pointy item that might grant invincibility ~ STAR
+A15 ^hint: This shape: ★
+
+A16. "Rolling in the Deep" singer ~ ADELE
+A16 ^hint: Name hidden in the phrase "grade level"
+
+A17. Looter-shooter whose name evokes a lack of free will (13-Across, "despite jumping around a lot") ~ DESTINY
+A17 ^hint: Synonym for "fate" formed by an anagram of "send it" + Y
+A17 ^refs: A13 A28 D5 D25
+
+A20. "Spelunky" spaces (35-Across) ~ CAVERNS
+A20 ^hint: Underground spaces with stalagmites
+A20 ^refs: A35 D20 D30 D36
+
+A21. Not homo- ~ HETERO
+A21 ^hint: Prefix meaning "different" spelled using only the letters in "other"
+
+A22. Letters on a lawyer's business card ~ ESQ
+A22 ^hint: "I passed the bar" abbreviation hidden in the phrase "sales quota"
+
+A25. ___ of Troy ("the face that launched a thousand ships") ~ HELEN
+A25 ^hint: Name formed by inserting a mother chicken into _ _ L E _
+
+A26. Large ice cream container ~ TUB
+A26 ^hint: Place to take a bath
+
+A28. Highest card in Pile-Up Poker (13-Across) ~ ACE
+A28 ^hint: Verb meaning "score 100% on" spelled with only musical notes
+A28 ^refs: A13 A17 D5 D25
+
+A29. Toothed tool ~ SAW
+A29 ^hint: Observed visually
+
+A32. Region ~ AREA
+A32 ^hint: Geometric measurement for a two-dimensional shape
+
+A33. Quick movement mechanic in 20-Down and 25-Down ~ DASH
+A33 ^hint: Small amount of salt
+A33 ^refs: D20 D25
+
+A35. See 13-Across ~ PARKOUR
+A35 ^hint: Urban obstacle-dodging activity formed by a public green space + a first-person plural possessive pronoun
+A35 ^refs: A13
+
+A37. "___ Is Betta Than Evvah!" (1976 ___ James album) ~ ETTA
+A37 ^hint: Name hidden in "dilettante"
+
+A38. Device with written versions of audiobooks ~ E|READER
+A38 ^hint: Digital alternative to a paperback or hardcover: Hyph.
+
+A39. Future houseplant ~ SEED
+A39 ^hint: Black or white bit in a watermelon
+
+A40. Last three in a set of 26 ~ X|Y|Z
+A40 ^hint: Sequence of letters that comes after UVW in the alphabet
+
+A41. Suffix in many language names ~ ESE
+A41 ^hint: Palindromic Spanish word hidden in "trade secret"
+
+
+D1. Nutrition plan ~ DIET
+D1 ^hint: Eating regimen that anagrams to "tide"
+
+D2. Real hero of "Frozen" ~ ANNA
+D2 ^hint: Palindromic name hidden in "human nature"
+
+D3. Part of a band's road schedule ~ TOUR|DATE
+D3 ^hint: Concert specification whose first word anagrams to "rout" and whose second word can mean "see romantically": 2 wds.
+
+D4. Cheeky body part ~ ASS
+D4 ^hint: Donkey
+
+D5. Card game whose name means "joker" in Latin (13-Across) ~ BALATRO
+D5 ^hint: Roman jester formed by an anagram of "lab rat" + O
+D5 ^refs: A13 A17 A28 D25
+
+D6. Entered smoothly and sneakily ~ SLID|IN
+D6 ^hint: Verb phrase meaning "crossed the threshold unnoticed" formed by verb similar to "glided" + the opposite of "out": 2 wds.
+
+D7. Huge ~ IMMENSE
+D7 ^hint: Synonym for "vast" spelled using only the letters in "mines"
+
+D8. Lace cloth that goes under a teacup ~ DOILY
+D8 ^hint: Delicate mat formed by D + a synonym for "greasy"
+
+D9. 90 degrees clockwise from norte ~ ESTE
+D9 ^hint: Name hidden in the phrase "sales team"
+
+D10. Parts of a city's grid: Abbr. ~ STS
+D10 ^hint: Abbreviation for some roads that's hidden in the phrase "East Side"
+
+D11. Leaders of D&D campaigns ~ D|MS
+D11 ^hint: Private social media messages formed by rearranging the consonants of "Amadeus": Abbr.
+
+D18. Numbers like 8, 16, and 64 ~ EVENS
+D18 ^hint: Opposites of odds
+
+D19. Spanish verb similar to "estar" ~ SER
+D19 ^hint: Spanish infinitive hidden in "fool's errand"
+
+D20. Precision platformer whose protagonist climbs a Vancouver Island mountain (35-Across) ~ CELESTE
+D20 ^hint: Name spelled using only the letters in "Celts"
+D20 ^refs: A20 A35 D30 D36
+
+D21. Greek goddess of witchcraft who's a maternal mentor in "25-Down II" ~ HECATE
+D21 ^hint: Mythological figure formed by an anagram of "teach" + E
+D21 ^refs: D25
+
+D23. Walked with a purpose ~ STRODE
+D23 ^hint: Verb meaning "proceeded boldly" that anagrams to "Dorset"
+
+D24. Waits Britishly ~ QUEUES
+D24 ^hint: Synonym for "lines" formed by inserting only vowels into Q _ _ _ _ S
+
+D25. God-dating(?) game set in a mythological underworld (13-Across) ~ HADES
+D25 ^hint: Greek god whose name becomes a word meaning "relief from the sunshine" if its last letter is moved to the front
+D25 ^refs: A13 A17 A28 D5
+
+D27. Ballet studio fixture ~ BARRE
+D27 ^hint: Dancer's rail that becomes a word meaning "naked" if one of its repeated letters is removed
+
+D30. Sliding-and-shooting game whose top players are called "predators" (35-Across) ~ APEX
+D30 ^hint: Synonym for "peak" whose first three letters spell a type of primate
+D30 ^refs: A20 A35 D20 D36
+
+D31. Cautious in the presence of danger ~ WARY
+D31 ^hint: Synonym for "hesitant" that anagrams to "awry"
+
+D32. Abbreviation before an alias ~ A|K|A
+D32 ^hint: Alter ego letters hidden in the name "Frida Kahlo"
+
+D34. Used to possess ~ HAD
+D34 ^hint: Owned
+
+D36. Trippy musical game whose name is also gaming lingo for "bring back to life" (35-Across) ~ REZ
+D36 ^hint: Shortened term for protected Native American land hidden in "pressure zone"
+D36 ^refs: A20 A35 D20 D30
 `,
   },
 ]
